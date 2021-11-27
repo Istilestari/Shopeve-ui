c123cc839f40df883e984be022c203bf7a4d783f
Istilestari <84213741+Istilestari@users.noreply.github.com>
2021-11-27 12:37:15 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.samsung.com/is/image/samsung/p6pim/id/ua65au9000kxxd/gallery/id-crystal-uhd-au9000-ua65au9000kxxd-423135784?$684_547_PNG$",
+        title: "LIVING ROOM DISC",
+        desc: "ALL THE LIVING ROOM PART THAT HAVE DISCOUNT. CODE(LR123)",
+        bg: "FFEFFF",
+    },    
+    {
+        id: 2,
+        img: "https://i.ibb.co/kxXpVsZ/RF.png",
+        title:"KITCHEN DISC",
+        desc: "ALL THE KITCHEN PART THAT HAVE DISCOUNT. CODE(KCN123)",
+        bg: "FFEFFF",
+    },    
+    {
+        id: 3,
+        img: "https://i.ibb.co/Twz6skJ/medium01-jpg-11-removebg-preview.png",
+        title: "LAUNDRY DISC",
+        desc: "ALL THE LAUNDRY PART THAT HAVE DISCOUNT. CODE(LDR123)",
+        bg: "FFEFFF",
+    },    
+];
+
+
+export const categories = [
+    {
+        id: 1,
+        img:"https://img.freepik.com/free-photo/tv-cabinet-white-wall-living-room-with-armchair-minimal-design-3d-rendering_41470-3752.jpg?size=338&ext=jpg",
+        tittle: "Living Room Elecronics",
+    },
+    {
+        id: 2,
+        img:"https://images.unsplash.com/photo-1556911220-bff31c812dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=968&q=80",
+        tittle: "Kitchen Part",
+    },
+    {
+        id: 3,
+        img:"https://images.unsplash.com/photo-1626806787461-102c1bfaaea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+        tittle: "Laundry Set",
+    },
+
+
+];
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Cngsxht/tva1.png",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/yW2LmpR/tv2.webp",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/nsW6W6c/tva2.png",
+    },
+    {
+        id: 4,
+        img: "https://images.samsung.com/is/image/samsung/p6pim/id/ua65au9000kxxd/gallery/id-crystal-uhd-au9000-ua65au9000kxxd-423135784?$684_547_PNG$",
+    },
+    {
+        id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-tv-4a-43-horizon/specs-header.png",
+    },
+    {
+        id: 5,
+        img: "https://images.samsung.com/is/image/samsung/p6pim/id/qa55qn700akxxd/gallery/id-neo-qled-8k-qn700a-qa55qn700akxxd-503903719?$684_547_PNG$",
+    },
+    {
+        id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-tv-4a-32-horizon/specs01.png",
+    },
+];
